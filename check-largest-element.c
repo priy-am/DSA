8916abbb9fd59193f799daf24a359e82c577f5dc
priy-am <priyam2101753@gmail.com>
2024-09-11 00:59:55 +0530

    correct 4th question

--- check-largest-element.c
+++ check-largest-element.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void swap(int *a, int *b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+};
+
 int main(int argc, char const *argv[])
 {
-    int len;
+    int len, k;
     printf("Enter the length of array:- ");
     scanf("%d", &len);
     int *array = (int *)malloc(len * sizeof(int));
@@ -17,16 +23,27 @@ int main(int argc, char const *argv[])
         scanf("%d",&array[i]);
     }
 
-    //find the kth largest element in the array
-    int largestElement;
-    largestElement = array[0];
     for (int i = 0; i < len; i++){
-        if(largestElement < array[i]){
-        largestElement = array[i];
+        for(int j= 0; j <len-i-1; j++){
+            if(array[j]<array[j+1]){
+                swap(&array[j], &array[j+1]);
+            }
         }
     }
-    
-    printf("The largest element in the array :- %d ", largestElement);
 
+    //displaying
+    for (int i = 0; i < len; i++){
+        printf("%d\t", array[i]);
+    }
+    
+    do{
+        printf("\nEnter the kth potion of the array that you want to know to;- ");
+        scanf("%d",&k);
+        if(k>5 || k<0){
+        printf("please enter the valid potion ");
+        }
+    } while (k>5 || k<0 || k == NULL);
+
+    printf("%dth largest element in the given array is :- %d", k, array[k]);
     return 0;
 }
